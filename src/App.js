fc067aa175bbd4f5d1fbf9342e3e6f6a2234c92c
dadasoda <0415rud@naver.com>
2024-06-18 11:40:42 +0900

    전체파일업로드

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+
+const App = () => {
+
+  const [movies,setMovies] = useState([
+    {},
+    {},
+    {}
+  ])
+
+  return (
+    <div>
+      <h1>리액트 시작하기</h1>
+    </div>
+  )
+}
+
+export default App
